0c7b1f09127ce4086088174406c4ac33f813a61d
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-21 14:32:32 -0400

    commit tree fc1f49ee51e446b879c09e19f5f3f2aa0a387d09

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(array) {
+    let winner;
+    let loser;
+    if (array.find(str => str.result === "W")) {
+        winner = array.find(str => str.result === "W");
+        return winner.year
+    }
+    else {
+        loser = array.find(str => str.result === "W")
+    }
+
+}
